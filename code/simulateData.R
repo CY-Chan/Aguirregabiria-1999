4899e3ac89d2a522d4d10db5055140920a00ab35
CY-Chan <j-chan@kellogg.northwestern.edu>
2014-11-26 10:39:18 -0600

    I think chebappxf is working correctly now.

--- code/simulateData.R
+++ code/simulateData.R
@@ -22,12 +22,13 @@ CCP<- data.frame(
 )
 
 # Testing chebappxf. Update key to i later and figure out where to get s.
-# The problem now is how to pull the correct CCP value out of the table.
 f<- function(key){
   CCP$prob_order[CCP$key==key]
 }
 
 f_tilda<- chebappxf(f, dims, c(0,Q))
-f_tilda(5.65263096)  #When this returns 0.690786510 we know we're using chebappxf correctly.
-evalongrid(f_tilda,dims)
+f_tilda(5.65263096)  #When this returns the corresponding CCP we know we're using chebappxf correctly.
+CCP$prob_order[6] #Corresponding CCP
+f_tilda(5.7) #Especially when this is also giving a close result!
+evalongrid(f_tilda,grid = expand.grid(chebknots(dims,c(0,Q)))) #And this returns the exact corresponding values.
 
