8431f6ef45ecf322b56de8738aca65c1dcd27687
CY-Chan <j-chan@kellogg.northwestern.edu>
2014-11-27 13:32:50 -0600

    Added initial CCP estimates

--- code/modules/estimatePrimitives.R
+++ code/modules/estimatePrimitives.R
@@ -12,3 +12,16 @@ estimateLogNormal<- function(){
                    control = list(fnscale = -1) # maximization option
   )
 }
+
+# Input: Dataframe of inventory and sales
+# Output: Dataframe of CCPs corresponding to different (i-s) levels
+initialCCPs<- function(data){
+  regData<- data.frame(replace<- 1 * (data$q > 0),
+                       key<- data$i - data$s)
+  results<- npreg(replace ~ key, data = regData)
+  output<- data.frame(key = results$eval, CCPs = results$mean) %>% 
+    unique %>%
+    arrange(key) %>%
+    select(key,CCPs)
+}
+
