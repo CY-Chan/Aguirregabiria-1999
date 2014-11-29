aa441d4f991a96344fa9b3aae7ba62d5f116ff9e
Matt Beamer <matthewbeamer2018@u.northwestern.edu>
2014-11-29 16:41:08 -0600

    Iterate PMLE til CCPs converge, check

--- code/modules/estimatePrimitives.R
+++ code/modules/estimatePrimitives.R
@@ -1,39 +1,23 @@
-MLE<- function(par, data){
-  print(par)
-  log((data$i > data$s) * dlnorm(data$s, meanlog = par[1], sdlog = par[2])  + 
-        (data$i <= data$s) * (1 - plnorm(data$s, meanlog = par[1], sdlog = par[2]))) %>%
-    sum
-}
-
-estimateLogNormal<- function(){
-  results <- optim(runif(2), MLE, 
-                   data = data, 
-                   method = 'L-BFGS-B', lower = c(0.01,0.01), 
-                   control = list(fnscale = -1) # maximization option
-  )
-  results$par
-}
-
-# Input: Dataframe of inventory and sales
-# Output: Dataframe of CCPs corresponding to different (i-s) levels
-initialCCPs<- function(data){
-  regData <- data.frame(replace = 1 * (data$q > 0),
-                        key = data$i - data$s)
-  results <- npreg(replace ~ key, data = regData)
-  newdata <- data.frame(key = first(chebknots(dims = dims,intervals = c(0,Q))))
-  output <- data.frame(key = newdata$key, 
-                       prob_order = predict(results,newdata = newdata))
-}
-
-#This will mostly be taken out of the function and sourced in a separate file.
-estimatePrimitives <- function(){
+# estimateDemandParameters
+#
+# INPUT: data, the data from which parameters are to be estimated
+# OUTPUT: theta1, estimates of mu, sigma (demand parameters)
+estimateDemandParameters <- function(data){
+  MLE<- function(par, data){
+#   print(par)
+    log((data$i > data$s) * dlnorm(data$s, meanlog = par[1], sdlog = par[2])  + 
+          (data$i <= data$s) * (1 - plnorm(data$s, meanlog = par[1], sdlog = par[2]))) %>%
+      sum
+  }
   
-  
-#   par <- data.frame(mu = 0,
-#                    sigma = 0,
-#                    pr = 3,
-#                    alpha = .3,
-#                    eta = 4)
+  estimateLogNormal <- function(){
+    results <- optim(runif(2), MLE, 
+                     data = data, 
+                     method = 'L-BFGS-B', lower = c(0.01,0.01), 
+                     control = list(fnscale = -1) # maximization option
+    )
+    results$par
+  }
   theta1 <- rep(0,2)
   
   # Repeat until the optimization doesn't throw an error because of bad initial values
@@ -44,22 +28,40 @@ estimatePrimitives <- function(){
       break
     } 
   }
-  theta1[1] <- temp[1]
-  theta1[2] <- temp[2]
-  CCP <- initialCCPs(data)
-  
+  theta1 <- c(temp[1],temp[2])
+}
+
+# estimateCCPs
+#
+# INPUT: data, Dataframe of inventory and sales
+# OUTPUT: Dataframe of CCPs corresponding to different (i-s) levels
+estimateCCPs<- function(data){
+  regData <- data.frame(replace = 1 * (data$q > 0),
+                        key = data$i - data$s)
+  results <- npreg(replace ~ key, data = regData)
+  newdata <- data.frame(key = first(chebknots(dims = dims,intervals = c(0,Q))))
+  output <- data.frame(key = newdata$key, 
+                       prob_order = predict(results,newdata = newdata))
+}
+
+#estimateCostParameters
+#
+# INPUT: theta1, estimate of demand parameters
+#        CCP, initial CCP estimate 
+#        data, dataframe of observations
+#        conv.err, convergence criterion for CCP estimates
+# OUTPUT: theta2, cost parameter estimates
+
+estimateCostParameters <- function(theta1,CCP,data,conv.err = 10^-5){
   g.old <- chebappxf(g.init,dims = dims,intervals = c(0,Q), CCP = CCP) %>%
-    Vectorize
-  
+    Vectorize  
+  # Estimate of f(i) using current CCP estimates, for use in optimization
   f_hat <- function(theta1, theta2, g.old){
     chebappxf(f,dims = dims,intervals = c(0,Q), theta1 = theta1, theta2 = theta2, g.old =  g.old) %>% 
     Vectorize
-  }
-  
-  # Pseudo-Maximum Likelihood, doesn't yet work
+  }  
+  # Pseudo maximum likelihood
   PMLE <- function(theta2,data){
-    #theta2 =c(theta2[1],theta2[2],4)
-    print(theta2)
     log((data$q > 0) * g(data$i - data$s, theta2 = theta2, 
                          f.old = f_hat(theta1 = theta1, theta2 = theta2, g.old =  g.old))  + 
           (data$q == 0) * (1 - g(data$i - data$s, theta2 = theta2, 
@@ -67,12 +69,29 @@ estimatePrimitives <- function(){
       sum
   }
   
-  results <- optim(c(3,.3,4), PMLE, 
-                   data = data, 
-                   method = 'L-BFGS-B', lower = c(0.01,0.01,0.01),
-                   control = list(fnscale = -1) # maximization option
-  )
-  
+  # Iterate getting cost parameter estimates and updating CCP estimates until CCP estimates converge
+  delta <- 1 + conv.err
+  while(delta > conv.err) {
+    results <- try(optim(runif(3), PMLE, 
+                     data = data, 
+                     method = 'L-BFGS-B', lower = c(0.1,0.1,0.1),
+                     control = list(fnscale = -1) # maximization option
+    ),silent = TRUE)
+    # Check if bad initial values. End optimization and try again if true.
+    if ('try-error' %in% class(results)){
+      break
+    } else {
+    theta2 <- results$par
+    f.old <- chebappxf(f,dims,c(0,Q),theta1 = theta1, theta2 = theta2, g.old = g.old) %>%
+      Vectorize
+    g.new <- chebappxf(g,dims,c(0,Q),theta2 = theta2, f.old = f.old) %>%
+      Vectorize
+    test.vals <- runif(100,min = 0,max = Q)
+    delta <- max(abs(g.new(test.vals) - g.old(test.vals)))
+    g.old <- g.new
+    }
+}
+theta2
 }
 
 
