bc8fab5d8855d826141a80aa05c425e2b438896b
Matt Beamer <matthewbeamer2018@u.northwestern.edu>
2014-11-29 09:50:44 -0600

    Rename parameters, Start PMLE

--- code/modules/simulateData.R
+++ code/modules/simulateData.R
@@ -5,17 +5,19 @@
 # OUTPUT: simulated sample of N observations of {i,s,q} (data frame)
 
 simdata <- function(S,rel.err = 10^-3){
-  par<- list(mu = 1,
-             sigma = 1,
-             pr = 3,
-             alpha = .3,
-             eta = 4)
+#   par<- list(mu = 1,
+#              sigma = 1,
+#              pr = 3,
+#              alpha = .3,
+#              eta = 4)
+  theta1 <- c(1,1)
+  theta2 <- c(3,.3,4)
   
-  V <- getDoubleIntegratedValueFunction(par, rel.err)
+  V <- getDoubleIntegratedValueFunction(theta1 = theta1, theta2 = theta2, rel.err)
   
   data <- data.frame(
     i = c(runif(1,min = 0,max = Q),rep(0,S-1)),
-    d = rlnorm(S,meanlog = par$mu,sdlog = par$sigma)
+    d = rlnorm(S,meanlog = theta1[1],sdlog = theta1[2])
   ) %>%
     mutate(s = pmin(i,d),
            q = c(rbinom(1,1,V$g(first(i)-first(s))) * (Q - first(i) + first(s)),rep(0,S-1)))
